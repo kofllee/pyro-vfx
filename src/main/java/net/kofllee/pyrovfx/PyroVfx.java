35cf86e7f414f56a8e504fa9c8c27af3c676387c
kofllee <111866132+kofllee@users.noreply.github.com>
2026-05-13 03:38:15 +0300

    Intitial commit

---
+++ src/main/java/net/kofllee/pyrovfx/PyroVfx.java
@@ -0,0 +1,18 @@
+package net.kofllee.pyrovfx;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(PyroVfx.MOD_ID)
+public final class PyroVfx {
+    public static final String MOD_ID = "pyro_vfx";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public PyroVfx(IEventBus modEventBus, ModContainer modContainer) {
+        LOGGER.info("Pyro VFX initialized");
+    }
+}
